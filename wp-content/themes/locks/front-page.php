ec1a4cc1bceb26233ae8e6340536630924760c04
MarketHubb <chris@markethubb.com>
2021-08-31 11:01:47 -0400

    Update: Remove homepage sections: "Hot Deals", "Crime States", "Safe Showroom"

--- wp-content/themes/locks/front-page.php
+++ wp-content/themes/locks/front-page.php
@@ -17,20 +17,20 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <a name="top"></a>
         <main id="main" class="site-main" role="main">
+
+            <!-- NEW -->
+
             <!--Header Image Template Part-->
             <?php get_template_part( 'template-parts/home/content', 'hero'); ?>
             <!-- Reviews -->
             <?php get_template_part( 'template-parts/home/content', 'reviews'); ?>
             <!-- Services -->
             <?php get_template_part( 'template-parts/home/content', 'services'); ?>
-            <!--Bonded And Insured Template Part-->
-            <?php //get_template_part( 'template-parts/content', 'bonded-and-insured'); ?>
-            <!--Hot Deals Template Part-->
-            <?php get_template_part( 'template-parts/content', 'hot-deals'); ?>
-            <!--Safes Template Part-->
-            <?php get_template_part( 'template-parts/content', 'safes-home'); ?>
-            <!-- Crime Section Template Part -->
-            <?php get_template_part( 'template-parts/content', 'crime-section'); ?>
+            <!-- Featured -->
+            <?php get_template_part( 'template-parts/home/content', 'featured'); ?>
+
+            <!-- LEGACY -->
+
             <!--Testimonials Template Part-->
             <?php get_template_part( 'template-parts/content', 'testimonials'); ?>
             <!--Lock Services Template Part-->
