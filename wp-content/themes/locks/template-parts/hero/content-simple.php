c02f3fb4a86ad719cd0e134e127a9454823eb838
MarketHubb <chris@markethubb.com>
2021-11-16 08:00:21 -0500

    New: Simple, text-only hero banner for page templates

---
+++ wp-content/themes/locks/template-parts/hero/content-simple.php
@@ -0,0 +1,11 @@
+<div class="container-fluid" id="safe-categories-page">
+    <div class="wrapper">
+        <div class="row">
+            <div class="col text-center mt-5 mb-3 py-2">
+                <h1 class="page-title text-capitalize mb-3"><?php echo $args['heading']; ?></h1>
+                <p class="page-subtitle lead font-weight-bold d-none d-md-block"><?php echo $args['subheading']; ?></p>
+                <p class="page-subtitle lead font-weight-bold d-md-none d-lg-none d-xl-none mb-0 pb-0"><?php echo $args['subheading_mobile']; ?></p>
+            </div>
+        </div>
+    </div>
+</div>
