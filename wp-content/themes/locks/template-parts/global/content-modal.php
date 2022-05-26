82cee45f132a2af8e4f494183bf148a2de90ce66
MarketHubb <chris@markethubb.com>
2022-05-26 12:23:45 -0500

    Update: Improve layout of image in modal window for Used Safes product inquiry

--- wp-content/themes/locks/template-parts/global/content-modal.php
+++ wp-content/themes/locks/template-parts/global/content-modal.php
@@ -23,11 +23,11 @@
                             <p class="lead d-inline-block my-3 font-weight-bold modal-subtitle lh-base fs-4 border-bottom"></p>
                         </div>
                     </div>
-                    <div class="row align-items-center">
+                    <div class="row align-items-center w-100">
                         <div class="col-md-7">
                             <?php gravity_form( $form_id, $display_title = false, $display_description = false, $ajax = false, $tabindex="10", $echo = true ); ?>
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-5 mx-auto text-center">
                             <img src="" class="modal-image shadow" alt="" />
                         </div>
                     </div>
