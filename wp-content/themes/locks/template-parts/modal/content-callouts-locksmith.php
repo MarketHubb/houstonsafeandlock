e962d30212b55fac95acc5fd1b0ea4ec938cba56
MarketHubb <chris@markethubb.com>
2022-10-10 18:33:30 -0500

    Update: Improve modal form layout on large devices

--- wp-content/themes/locks/template-parts/modal/content-callouts-locksmith.php
+++ wp-content/themes/locks/template-parts/modal/content-callouts-locksmith.php
@@ -1,19 +1,19 @@
-<div class="container modal-callouts my-3">
+<div class="container modal-callouts my-3 mb-md-4">
     <div class="row">
         <div class="col-4 text-center">
             <i class="fa-solid fa-house-lock text-blue mb-2"></i>
             <p class="mb-0"><small class="text-secondary">Install</small></p>
-            <p class=" mb-0 text-secondary fw-600">Locks</p>
+            <p class="lh-1 mb-0 text-secondary fw-600">Locks</p>
         </div>
         <div class="col-4 text-center">
             <i class="fa-solid fa-key text-blue mb-2"></i>
             <p class="mb-0"><small class="text-secondary">Copy</small></p>
-            <p class=" mb-0  text-secondary fw-600">Keys</p>
+            <p class="lh-1 mb-0  text-secondary fw-600">Keys</p>
         </div>
         <div class="col-4 text-center">
             <i class="fa-solid fa-door-open text-blue mb-2"></i>
             <p class="mb-0"><small class="text-secondary">24/7</small></p>
-            <p class=" mb-0 text-secondary fw-600">Access</p>
+            <p class="lh-1 mb-0 text-secondary fw-600">Access</p>
         </div>
     </div>
 </div>
