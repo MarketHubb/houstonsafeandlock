120f833b8f826fec2728b49d5a5d4f004a96a71c
MarketHubb <chris@markethubb.com>
2023-06-13 10:13:52 -0500

    Update: Add GA4 tracking script to header

--- wp-content/themes/locks/header.php
+++ wp-content/themes/locks/header.php
@@ -52,8 +52,16 @@
     <script>  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-78250454-1', 'auto');  ga('send', 'pageview');</script>
     <!-- End HSL Google Analytics -->
 
-<!--    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"-->
-<!--          rel="stylesheet">-->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VGSRGWN5T7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VGSRGWN5T7');
+    </script>
+
 
     <script type="text/javascript">
         (function($) {
