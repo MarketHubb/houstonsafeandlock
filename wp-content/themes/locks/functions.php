98e3671adb61d8cb729591fbff7e305814b4853a
Chris Hubbard <chris@markethubb.com>
2024-03-06 11:34:04 -0600

    Fix: "All Safes" - Enqueue JS and CSS assets based on page template, not post id

--- wp-content/themes/locks/functions.php
+++ wp-content/themes/locks/functions.php
@@ -167,7 +167,7 @@ function locks_scripts()
 	wp_enqueue_script('locks-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
 	// Filter / Sort Safes page
-	if (is_page(8854)) {
+	if (is_page_template('page-templates/safes-new.php')) {
 		wp_enqueue_script('safes-new', get_stylesheet_directory_uri() . '/js/safes-new.js', ['jquery'], '1.0', true);
 		wp_enqueue_script('mixitup');
 		wp_enqueue_script('mixitup-controls');
