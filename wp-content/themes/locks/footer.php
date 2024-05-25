e5bbd21116be8a1f943434b42f8a801ba36316b3
Chris Hubbard <chris@markethubb.com>
2024-05-25 12:19:48 -0500

    Update: Add "one company, two locations" content section to footer

--- wp-content/themes/locks/footer.php
+++ wp-content/themes/locks/footer.php
@@ -12,6 +12,49 @@
 ?>
 </div><!-- #content -->
 
+<!-- One company, two locations -->
+
+<section class="tw-py-24">
+    <div class="container">
+
+        <div class="row justify-content-center pt-4 pb-5">
+            <div class="col-md-8 text-center">
+                <h2 class="">One company, two locations</h2>
+                <p class="lead fw-normal">We're one company, with two locations to better serve the safe and security needs of our valued Houston customers. <a href="<?php echo get_permalink(8988); ?>">Read more about the rebrand of King Safe & Lock here</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="row justify-content-between">
+            <div class="col-md-6">
+                <div class="card h-100 shadow-sm"><img src="<?php echo home_url() . '/wp-content/uploads/2023/06/HSL-Storefront.jpg'; ?>" class="card-img-top" alt="...">
+                    <div class="card-body bg-grey-light p-4">
+                        <h3 class="card-title">Houston Safe & Lock - Westheimer</h3>
+                        <p class="card-text">
+                            <strong> <a href="tel:713-522-5555">713-522-5555</a></strong><br>
+                            10210 Westheimer Rd.<br>
+                            Houston, Texas - 77042<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card h-100 shadow-sm"><img src="<?php echo home_url() . '/wp-content/uploads/2023/06/KSL-Showroom-4.jpg'; ?>" class="card-img-top" alt="...">
+                    <div class="card-body bg-grey-light p-4">
+                        <h3 class="card-title">Houston Safe & Lock - Memorial</h3>
+                        <p class="card-text">
+                            <strong> <a href="tel:713-465-0055">713-465-0055</a></strong><br>
+                            8429 Katy Fwy<br>
+                            Houston, Texas 77024<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
 <div class="footer-container bg-blue text-white py-5">
     <div class="container py-4 my-2">
         <div class="row">
@@ -168,7 +211,7 @@ if (get_field('is_sale_active', 'option')) {
 }
 
 if (is_page_template('page-templates/safes-new.php')) {
-    get_template_part( 'template-parts/modal/content', 'filters' );
+    get_template_part('template-parts/modal/content', 'filters');
 }
 
 if (is_shop() || is_archive() || is_singular('product') || is_page(3857) || is_page(6287) || is_page([6448, 6624, 6839, 7276, 7728, 8854]) || is_page_template('page-templates/full-width.php') || is_page_template('page-templates/safes-new.php')) {
