0e0fb64aca3568f16f221fd02fc04cf758f905d9
Chris Hubbard <chris@markethubb.com>
2024-10-07 17:04:20 -0500

    Update: Add footer include and js for new modal (pop-up) system

--- wp-content/themes/locks/footer.php
+++ wp-content/themes/locks/footer.php
@@ -14,49 +14,49 @@
 
 <?php if (!is_singular('product')) { ?>
 
-<!-- One company, two locations -->
-<section class="tw-py-24">
-    <div class="container">
+    <!-- One company, two locations -->
+    <section class="tw-py-24">
+        <div class="container">
 
-        <div class="row justify-content-center pt-4 pb-5">
-            <div class="col-md-8 text-center">
-                <h2 class="">One company, two locations</h2>
-                <p class="lead fw-normal">We're one company, with two locations to better serve the safe and security needs of our valued Houston customers. <a href="<?php echo get_permalink(8988); ?>">Read more about the rebrand of King Safe & Lock here</a>
-                </p>
-            </div>
-        </div>
-
-        <div class="row justify-content-between">
-            <div class="col-md-6">
-                <div class="card h-100  shadow-sm">
-                    <img src="<?php echo home_url() . '/wp-content/uploads/2023/06/HSL-Storefront.jpg'; ?>" class="card-img-top md:tw-h-[300px] tw-object-cover tw-object-center" alt="...">
-                    <div class="card-body bg-grey-light p-4">
-                        <h3 class="card-title">Houston Safe & Lock - Westheimer</h3>
-                        <p class="card-text">
-                            <strong> <a href="tel:713-522-5555">713-522-5555</a></strong><br>
-                            10210 Westheimer Rd.<br>
-                            Houston, Texas - 77042<br>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card h-100  shadow-sm">
-                    <img src="<?php echo home_url() . '/wp-content/uploads/2024/06/HSL-Memorial.webp'; ?>" class="card-img-top md:tw-h-[300px] tw-object-cover tw-object-center" alt="...">
-                    <div class="card-body bg-grey-light p-4">
-                        <h3 class="card-title">Houston Safe & Lock - Memorial</h3>
-                        <p class="card-text">
-                            <strong> <a href="tel:713-465-0055">713-465-0055</a></strong><br>
-                            8429 Katy Fwy<br>
-                            Houston, Texas 77024<br>
-                        </p>
-                    </div>
+            <div class="row justify-content-center pt-4 pb-5">
+                <div class="col-md-8 text-center">
+                    <h2 class="">One company, two locations</h2>
+                    <p class="lead fw-normal">We're one company, with two locations to better serve the safe and security needs of our valued Houston customers. <a href="<?php echo get_permalink(8988); ?>">Read more about the rebrand of King Safe & Lock here</a>
+                    </p>
                 </div>
             </div>
 
+            <div class="row justify-content-between">
+                <div class="col-md-6">
+                    <div class="card h-100  shadow-sm">
+                        <img src="<?php echo home_url() . '/wp-content/uploads/2023/06/HSL-Storefront.jpg'; ?>" class="card-img-top md:tw-h-[300px] tw-object-cover tw-object-center" alt="...">
+                        <div class="card-body bg-grey-light p-4">
+                            <h3 class="card-title">Houston Safe & Lock - Westheimer</h3>
+                            <p class="card-text">
+                                <strong> <a href="tel:713-522-5555">713-522-5555</a></strong><br>
+                                10210 Westheimer Rd.<br>
+                                Houston, Texas - 77042<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card h-100  shadow-sm">
+                        <img src="<?php echo home_url() . '/wp-content/uploads/2024/06/HSL-Memorial.webp'; ?>" class="card-img-top md:tw-h-[300px] tw-object-cover tw-object-center" alt="...">
+                        <div class="card-body bg-grey-light p-4">
+                            <h3 class="card-title">Houston Safe & Lock - Memorial</h3>
+                            <p class="card-text">
+                                <strong> <a href="tel:713-465-0055">713-465-0055</a></strong><br>
+                                8429 Katy Fwy<br>
+                                Houston, Texas 77024<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php } ?>
 <div class="footer-container bg-blue text-white py-5">
     <div class="container py-4 my-2">
@@ -327,6 +327,13 @@ if (is_shop() || is_archive() || is_singular('product') || is_page(3857) || is_p
         });
     </script>
 <?php } ?>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        HSOverlay.autoInit();
+    });
+</script>
+<?php get_template_part('template-parts/tw-shared/content', 'modal-global') ?>
 
 </body>
 
