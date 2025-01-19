6919b9b425480895d9803f3171ac824e174501c9
MarketHubb <chris@markethubb.com>
2025-01-19 00:57:33 -0600

    Fix: Remove "static" declaration on lead form modal to ensure it can be scrolled on mobile

--- wp-content/themes/locks/template-parts/product/content-modal.php
+++ wp-content/themes/locks/template-parts/product/content-modal.php
@@ -1,4 +1,4 @@
-<div id="hs-full-screen-modal-below-md" class="--overlay-backdrop:static hs-overlay hidden size-full fixed top-0 start-0 z-[99999001] overflow-x-hidden overflow-y-auto pointer-events-none bg-white sm:bg-transparent" role="dialog" tabindex="-1" aria-labelledby="hs-full-screen-modal-below-md-label">
+<div id="hs-full-screen-modal-below-md" class="hs-overlay hidden size-full fixed top-0 start-0 z-[99999001] overflow-x-hidden overflow-y-auto pointer-events-none bg-white sm:bg-transparent" role="dialog" tabindex="-1" aria-labelledby="hs-full-screen-modal-below-md-label">
     <div class="hs-overlay-open:mt-0 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-10 opacity-0 transition-all max-w-full max-h-full h-full md:hs-overlay-open:mt-10 md:mt-0 md:max-w-3xl md:max-h-none md:h-auto md:mx-auto ">
         <div class="flex flex-col bg-white pointer-events-auto max-w-full max-h-full h-full md:max-h-none md:h-auto md:rounded-xl md:shadow-sm">
 
