efa855bcdaee4458fe7b49d84c8aca0ea434cbe9
MarketHubb <chris@markethubb.com>
2025-01-19 02:08:11 -0600

    Fix: Enable scrolling of modal window on mobile devices

--- wp-content/themes/locks/template-parts/product/content-modal.php
+++ wp-content/themes/locks/template-parts/product/content-modal.php
@@ -1,19 +1,18 @@
-<div id="hs-full-screen-modal-below-md" class="hs-overlay hidden size-full fixed top-0 start-0 z-[99999001] overflow-x-hidden overflow-y-auto pointer-events-none bg-white sm:bg-transparent" role="dialog" tabindex="-1" aria-labelledby="hs-full-screen-modal-below-md-label">
-    <div class="hs-overlay-open:mt-0 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-10 opacity-0 transition-all max-w-full max-h-full h-full md:hs-overlay-open:mt-10 md:mt-0 md:max-w-3xl md:max-h-none md:h-auto md:mx-auto ">
-        <div class="flex flex-col bg-white pointer-events-auto max-w-full max-h-full h-full md:max-h-none md:h-auto md:rounded-xl md:shadow-sm">
+<div id="hs-full-screen-modal-below-md" class="hs-overlay hidden size-full fixed top-0 start-0 z-[99999001] overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-scroll-inside-body-modal-label">
+    <div class="sm:hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg md:max-w-3xl sm:w-full sm:m-3 h-full sm:h-[calc(100%-3.5rem)] sm:mx-auto">
+        <div class="max-h-full overflow-hidden flex flex-col bg-white border shadow-sm sm:rounded-xl pointer-events-auto">
 
-                <?php get_template_part('template-parts/product/content', 'modal-close'); ?>
+            <?php get_template_part('template-parts/product/content', 'modal-close'); ?>
 
-                <div class="px-4 sm:px-0" id="product-lead-modal">
+            <div class="px-4 sm:px-0 overflow-y-auto" id="product-lead-modal">
 
-                    <?php get_template_part('template-parts/product/content', 'modal-header'); ?>
+                <?php get_template_part('template-parts/product/content', 'modal-header'); ?>
 
-                    <?php get_template_part('template-parts/product/content', 'modal-lead'); ?>
+                <?php get_template_part('template-parts/product/content', 'modal-lead'); ?>
 
-                </div>
+            </div>
 
-                <?php get_template_part('template-parts/product/content', 'modal-footer'); ?>
-
+            <?php get_template_part('template-parts/product/content', 'modal-footer'); ?>
 
         </div>
     </div>
