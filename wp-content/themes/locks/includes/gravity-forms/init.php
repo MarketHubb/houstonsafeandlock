e5a6fc655756f9a48ad0d66522c4dcb748d8776c
MarketHubb <chris@markethubb.com>
2025-04-09 14:51:39 -0500

    FIX: Remove admin init scripts preventing proper JSON response from server

--- wp-content/themes/locks/includes/gravity-forms/init.php
+++ wp-content/themes/locks/includes/gravity-forms/init.php
@@ -2,4 +2,4 @@
 defined('ABSPATH') || exit;
 require_once "hooks.php";
 require_once "helpers.php";
-require_once "admin.php";
+// require_once "admin.php";
--- wp-content/themes/locks/includes/shared/init.php
+++ wp-content/themes/locks/includes/shared/init.php
@@ -2,5 +2,5 @@
 defined('ABSPATH') || exit;
 
 require_once "enqueue.php";
-require_once "admin.php";
+// require_once "admin.php";
 require_once "helpers.php";
