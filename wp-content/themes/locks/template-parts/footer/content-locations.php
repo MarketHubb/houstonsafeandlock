b7b86c01c8410eea5945a1c2b1887470a29c880b
MarketHubb <chris@markethubb.com>
2025-05-06 09:50:42 -0500

    Update: Remove reference to "Houston Safe & Lock - Memorial" and link to blog post from footer

--- wp-content/themes/locks/template-parts/footer/content-locations.php
+++ wp-content/themes/locks/template-parts/footer/content-locations.php
@@ -4,8 +4,6 @@
         <div class="flex justify-center pt-4 pb-5">
             <div class="md:w-7/12 text-center pb-12">
                 <h2>One company, two locations</h2>
-                <p class="text-lg font-normal">We're one company, with two locations to better serve the safe and security needs of our valued Houston customers. <a href="<?php echo get_permalink(8988); ?>">Read more about the rebrand of King Safe & Lock here</a>
-                </p>
             </div>
         </div>
 
@@ -16,7 +14,7 @@
                         class="w-full md:h-[300px] object-cover object-center rounded-t-lg"
                         alt="...">
                     <div class="p-4 bg-gray-50">
-                        <h3 class="text-xl font-semibold mb-2">Houston Safe & Lock - Westheimer</h3>
+                        <h3 class="text-xl font-semibold mb-2">Houston Safe & Lock</h3>
                         <p>
                             <strong><a href="tel:713-522-5555" class="hover:text-gray-600">713-522-5555</a></strong><br>
                             10210 Westheimer Rd.<br>
@@ -31,7 +29,7 @@
                         class="w-full md:h-[300px] object-cover object-center rounded-t-lg"
                         alt="...">
                     <div class="p-4 bg-gray-50">
-                        <h3 class="text-xl font-semibold mb-2">Houston Safe & Lock - Memorial</h3>
+                        <h3 class="text-xl font-semibold mb-2">King Safe & Lock</h3>
                         <p>
                             <strong><a href="tel:713-465-0055" class="hover:text-gray-600">713-465-0055</a></strong><br>
                             8429 Katy Fwy<br>
